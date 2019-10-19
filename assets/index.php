d24313b2281debe097b7bde97790c5a0bd06d04d
taek <taek@taek.us>
2019-10-19 13:06:40 -0700

    Files renamed and changed didn't get added :\

---
+++ assets/index.php
@@ -0,0 +1,16 @@
+<?php
+/**
+	Project Name: Butthurt Form
+	Description: Some little project of some sort for IRC
+	Author: Gabriel Roach
+	Author URI: https://taek.us/
+	Project URI: http://git.taek.us/Taek/bhf
+	Demo URI: http://taek.us/dev/bhf/
+	License: GNU Affero General Public License v3.0
+**/
+
+	if (!defined("base")) {
+ 		header($_SERVER["SERVER_PROTOCOL"] . " 404 Not Found");
+ 		exit;
+ 	}
+?>
